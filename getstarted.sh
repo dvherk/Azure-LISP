0945d9c91c4515fdd374dfae206c6a32caa4a73b
Dennis van Herk <devanher@microsoft.com>
2019-04-19 20:25:22 +0200

    updated Azure schema API versions

--- getstarted.sh
+++ getstarted.sh
@@ -1,12 +1,12 @@
 # Deploy lab
-url=https://raw.githubusercontent.com/Microsoft/Azure-LISP/master/lisp_lab_iterate_through_vnet.json
+url=https://raw.githubusercontent.com/dvherk/Azure-LISP/master/lisp_lab_iterate_through_vnet.json
 
-rg=lisplab
-az group create -n $rg -l eastus
+rg=LISPdemo
+az group create -n $rg -l westeurope
 az group deployment create -g $rg --template-uri $url --parameters '{"adminPassword":{"value":"Msft123Msft123"}}'
 
 # Check Public IP addresses
 az network public-ip list -g $rg --query [].[name,ipAddress] -o tsv
 
 # Cleanup
-az group delete -n $rg -y --no-wait
+#az group delete -n $rg -y --no-wait
